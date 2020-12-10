79bc54eac1529a41790f840546d83b5ce1358b2c
ttnam99 <51222429+ttnam99@users.noreply.github.com>
2020-12-10 17:21:00 +0700

    Add User Schema, Update user route,auth route

--- models/user.model.js
+++ models/user.model.js
@@ -1,53 +1,70 @@
 const db = require('../utils/db');
 
 const userSchema = require('../schemas/user.schema');
-
+const validator = require('validator');
 const userModel = db.model('users', userSchema);
 
 module.exports = {
     async singleById(id) {
-        const user = await userModel.findOne({ _id: id }).exec();
+        const user = await userModel.findOne({
+            _id: id
+        }).exec();
         return user;
     },
 
     async singleByEmail(_email) {
-        const user = await userModel.findOne({ email: _email }).exec();
-        console.log(user);
+        if (!validator.isEmail(_email)) {
+            throw new Error('Email is invalid');
+        }
+        const user = await userModel.findOne({
+            email: _email
+        }).exec();
+        if (!user) {
+            throw new Error('Unable to find user with email ' + email);
+        }
+        //console.log(user);
         return user;
     },
 
     updateRefreshToken(id, refreshToken) {
-        return userModel.updateOne({ _id: id }, { rfToken: refreshToken });
+        return userModel.updateOne({
+            _id: id
+        }, {
+            rfToken: {
+                token: refreshToken,
+                createdAt: Date.now() / 1000
+            }
+        });
     },
 
     async isRefreshTokenExisted(id, refreshToken) {
-        const result = await userModel.findOne({ _id: id, rfToken: refreshToken });
+        const result = await userModel.findOne({
+            _id: id,
+            "rfToken.token": refreshToken
+        });
         if (result === null) return false;
         return true;
     },
 
     async add(user) {
-        console.log(userModel.schema);
-        const userObj = new userModel({
-            email: user.email,
-            password: user.password,
-            fullname: user.fullname,
-            role: user.role,
-            avatar: '',
-            description: '',
-            favorite_list: [],
-            join_list: [],
-            course_list: [],
-            rfToken: ''
-        });
-        await userObj.save(function (err, userObj) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('saved successfully:', userObj);
-                return userObj._id;
-            }
-        });
-        
+        try {
+            //console.log(userModel.schema);
+            const userObj = new userModel({
+                email: user.email,
+                password: user.password,
+                fullname: user.fullname,
+                role: user.role,
+                rfToken: {
+                    token: ''
+                }
+            });
+            await userObj.save();
+            return userObj._id;
+        } catch (error) {
+            res.status(400).send({
+                error
+            })
+        }
+
     }
 };
--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,9 @@
 const db = require('mongoose');
 
-db.connect('mongodb+srv://admin:9AnE6wSwY8baPYU@wnc.nhw5i.mongodb.net/online-cademy?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+db.connect('mongodb+srv://admin:9AnE6wSwY8baPYU@wnc.nhw5i.mongodb.net/online-academy?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(() => {
+    console.log('Failed to connect to database');
+});
 module.exports = db;
